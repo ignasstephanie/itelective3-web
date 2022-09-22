bc893858e9225008e8ae667f5150798f681ab15a
ignasstephanie <ignasstephanie@gmail.com>
2022-09-23 00:28:16 +0800

    displayed data from weather-js

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ var weather = require('weather-js');
 app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
 
-    weather.find({ search: 'Davao, PH', degreeType: 'C' }, function (err, result) {
+    weather.find({ search: 'Davao City, PH', degreeType: 'C' }, function (err, result) {
         var weather_data = null;
         if (err) console.log(err);
         else {
-			console.log(JSON.stringify(result, null, 2))
+            // console.log(JSON.stringify(result, null, 2));
             weather_data = result;
         }
-        res.render('index', { weather: weather_data });
+        res.render('index', { title: "Home", weather: weather_data });
     });
 
 })
 app.get('/other', function (req, res) {
-    res.render('other');
+    res.render('other', {title: "Other"});
 })
 
 
