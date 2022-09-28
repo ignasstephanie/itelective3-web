e87a4e084c0d34c8084612c1b7a50b5ebad22fcd
ignasstephanie <ignasstephanie@gmail.com>
2022-09-28 15:10:28 +0800

    updated index and installed fetch API

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 var weather = require('weather-js');
-
+var fetchUrl = require('fetch').fetchUrl;
 app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
 
@@ -11,16 +11,17 @@ app.get('/', function (req, res) {
         var weather_data = null;
         if (err) console.log(err);
         else {
-            // console.log(JSON.stringify(result, null, 2));
+            console.log(JSON.stringify(result, null, 2));
             weather_data = result;
         }
         res.render('index', { title: "Home", weather: weather_data });
     });
-
-})
-app.get('/other', function (req, res) {
-    res.render('other', {title: "Other"});
-})
-
-
+});
+app.get('/other', async function (req, res) {
+    var data ={
+        url: req.url,
+        itemData: []
+    }
+    res.render('other', data)    
+});
 app.listen(8000);
